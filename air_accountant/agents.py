c9cf8db7b54555ffa7f64072f4276f961284283f
deejungx <dipzz1394@gmail.com>
2024-01-08 21:33:11 +0545

    Fix gitignore and logging in airtable.py

--- air_accountant/agents.py
+++ air_accountant/agents.py
@@ -1,27 +1,3 @@
-"""
-Agent 1: Accountant
-You are an expert accountant. You are meticulous, analytical, and have a strong understanding of accounting principles. You are also a great communicator and can explain complex accounting concepts to non-accountants.
-Your task is to gather all the information needed on financial transactions for accurately recording the transactions and journal entries.
-Please make sure you complete the objective above with the following rules:
-1. You should follow the double entry method for recording transactions. This means that each transaction should have at least two accounts affected. You will record these as separate transactions and link them to the same journal entry.
-2. Don't make assumptions. Ask for clarification if the user's information is ambiguous.
-3. Before you make any decisions, you should fetch the list of accounts in the chart of accounts to help identify which accounts are impacted by the transaction.
-4. Each transaction record should include a date, description, amount, account, and type.
-5. You should find out information on the description, date and amount of the transactions from the user. You should keep asking questions until you have all the information you need.
-6. After gathering enough information, you should think and identify which accounts are impacted by the transactions. You should also determine the type of entry (debit or credit) for each transaction.
-7. Each journal entry should include a name and list of transactions. You should come up with the name for the journal entry.
-8. After determining all of the information, you should confirm with the user that the information is correct.
-9. Once you receive approval from the user, you will hand over the transactions and journal entries to the bookkeeper.
-
-
-Agent 2: Bookkeeper
-You are an expert bookkeeper. You are organized, resourceful and detail oriented.
-Your task is to record the financial transactions and journal entries provided to you by the Accountant to Airtable.
-You should only use the information provided by the Accountant to record the transactions and journal entries. You should not make any assumptions or changes to the information provided by the Accountant.
-Please use the following steps to complete the task:
-1. You should first record the transactions to Airtable. After successfully recording the transactions, you should receive the same information back with transaction_id included.
-2. You will use the transaction_id along with the journal entry name and date to record the journal entry to Airtable.
-"""
 from autogen import config_list_from_json, UserProxyAgent, GroupChat, GroupChatManager
 from autogen.agentchat.contrib.gpt_assistant_agent import GPTAssistantAgent
 from dotenv import load_dotenv
@@ -40,7 +16,7 @@ bookkeeper_id = os.getenv("BOOKKEEPER_ASSISTANT_ID")
 user_proxy = UserProxyAgent(
     name="UserProxy",
     is_termination_msg=lambda msg: "TERMINATE" in msg["content"],
-    human_input_mode="TERMINATE",
+    human_input_mode="ALWAYS",
     max_consecutive_auto_reply=1,
 )
 
@@ -63,7 +39,7 @@ bookkeeper = GPTAssistantAgent(
 
 bookkeeper.register_function(
     function_map={
-        "record_transaction": record_bulk_transaction,
+        "record_transactions": record_bulk_transaction,
         "record_journal_entry": record_journal_entry,
     }
 )
--- air_accountant/airtable.py
+++ air_accountant/airtable.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from dotenv import load_dotenv
@@ -5,6 +6,7 @@ from pyairtable import Api
 
 
 load_dotenv()  # load environment variables from .env file
+logger = logging.getLogger(__name__)
 
 
 def create_api_connection():
@@ -24,9 +26,12 @@ def get_all_accounts():
     base_id = os.getenv("AIRTABLE_BASE_ID")
     table_name = os.getenv("ACCOUNTS_TABLE_ID")
     accounts_table = api.table(base_id=base_id, table_name=table_name)
+    logger.debug(f"Fetching all accounts from table_id: {table_name}")
     result = accounts_table.all()  # get all records from the table
+    logger.info(f"Fetched {len(result)} accounts")
     accounts = [
         {
+            "id": acc["fields"]["acc_number"],
             "name": acc["fields"]["name"],
             "description": acc["fields"]["description"],
             "type": acc["fields"]["type"],
@@ -63,8 +68,12 @@ def record_bulk_transaction(transactions):
     api = create_api_connection()
     base_id = os.getenv("AIRTABLE_BASE_ID")
     table_name = os.getenv("TRANSACTIONS_TABLE_ID")
+    logger.debug(
+        f"Recording transactions to table_id: {table_name}\n\ntransactions:\n\n{transactions}"
+    )
     transactions_table = api.table(base_id=base_id, table_name=table_name)
     tx_result = transactions_table.batch_create(transactions)
+    logger.info(f"Recorded {len(tx_result)} transactions")
     return tx_result
 
 
